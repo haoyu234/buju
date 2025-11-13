201813b8ab3d0154f5a13e2e147f3ef3c96df17d
haoyu234 <164346864@qq.com>
2025-11-13 21:02:15 +0800

    Preallocate memory to avoid checking capacity on each `add` call

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 nim c -d:release -d:danger bench.nim
-nim c -d:release example.nim
+
+# nim c -d:release example.nim
 
 # nim js -d:danger -d:release tools/viewer/app.nim
 # python assets/tpl.py
