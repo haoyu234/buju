14577267b5d6ab9986e8465d33137e9fd9b1f0e5
haoyu234 <164346864@qq.com>
2025-11-13 21:04:34 +0800

    update `removeChild` test case

--- tests/tsibling.nim
+++ tests/tsibling.nim
@@ -6,10 +6,9 @@ import buju/core
 template getAddr(body): auto =
   when NimMajor > 1: body.addr else: body.unsafeAddr
 
-proc check2(
-    l: ptr Context, n, firstChild, lastChild, prevSibling, nextSibling: NodeID
-) =
-  let n = l.node(n)
+proc check2(l: ptr Context, n, firstChild, lastChild, prevSibling,
+    nextSibling: NodeID) =
+  let n = node(l, n)
   check n.firstChild == firstChild
   check n.lastChild == lastChild
   check n.prevSibling == prevSibling
@@ -50,28 +49,45 @@ test "removeChild":
   let node1 = l.node()
   let node2 = l.node()
   let node3 = l.node()
+  let node4 = l.node()
 
   l.insertChild(root, node1)
   l.insertChild(root, node2)
   l.insertChild(root, node3)
+  l.insertChild(root, node4)
 
+  # _x_
   l.removeChild(root, node2)
 
-  check2(l.getAddr, root, node1, node3, NIL, NIL)
+  check2(l.getAddr, root, node1, node4, NIL, NIL)
   check2(l.getAddr, node1, NIL, NIL, NIL, node3)
   check2(l.getAddr, node2, NIL, NIL, NIL, NIL)
-  check2(l.getAddr, node3, NIL, NIL, node1, NIL)
+  check2(l.getAddr, node3, NIL, NIL, node1, node4)
+  check2(l.getAddr, node4, NIL, NIL, node3, NIL)
 
+  # ^x_
   l.removeChild(root, node1)
+
+  check2(l.getAddr, root, node3, node4, NIL, NIL)
+  check2(l.getAddr, node1, NIL, NIL, NIL, NIL)
+  check2(l.getAddr, node2, NIL, NIL, NIL, NIL)
+  check2(l.getAddr, node3, NIL, NIL, NIL, node4)
+  check2(l.getAddr, node4, NIL, NIL, node3, NIL)
+
+  # _x$
+  l.removeChild(root, node4)
 
   check2(l.getAddr, root, node3, node3, NIL, NIL)
   check2(l.getAddr, node1, NIL, NIL, NIL, NIL)
   check2(l.getAddr, node2, NIL, NIL, NIL, NIL)
   check2(l.getAddr, node3, NIL, NIL, NIL, NIL)
+  check2(l.getAddr, node4, NIL, NIL, NIL, NIL)
 
+  # ^x$
   l.removeChild(root, node3)
 
   check2(l.getAddr, root, NIL, NIL, NIL, NIL)
   check2(l.getAddr, node1, NIL, NIL, NIL, NIL)
   check2(l.getAddr, node2, NIL, NIL, NIL, NIL)
   check2(l.getAddr, node3, NIL, NIL, NIL, NIL)
+  check2(l.getAddr, node4, NIL, NIL, NIL, NIL)
